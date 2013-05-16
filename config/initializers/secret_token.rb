6dface7331e9b98d8beeb6230748ab90fe5dd688
João Daniel <jdanielnd@gmail.com>
2013-05-16 11:08:01 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SidekiqDynamicPoc::Application.config.secret_token = '8153827062ec97fb4e28e264a3daff1e7b08989f05682506d1bdbe57baf482bc8a2dd9dde0aea8c15db158cfc1eacde89a8795cb03d9a93d42d5975372aaf9a5'
